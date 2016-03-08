ee921e0fb7d0c74492175746cc50498980fe65bb
pcmquizon <camillemendoza17@gmail.com>
2016-03-08 17:12:35 +0800

    Add register user

--- controllers/user.js
+++ controllers/user.js
@@ -4,23 +4,28 @@ const mysql   = require('anytv-node-mysql');
 const winston = require('winston');
 
 /**
- * @api {get} /user/:id Get user information
- * @apiName GetUser
+ * @api {post} /user Create new user
+ * @apiName CreateUser
  * @apiGroup User
  *
- * @apiParam {String} id User's unique ID
+ * @apiParam {String} email User's email
+ * @apiParam {String} password User's password
+ * @apiParam {String} first_name User's first name
+ * @apiParam {String} middle_initial User's middle initial
+ * @apiParam {String} last_name User's last name
  *
- * @apiSuccess {String} user_id User's unique ID
- * @apiSuccess {String} date_created Time when the user was created
- * @apiSuccess {String} date_updated Time when last update occurred
+ * @apiSuccess 
  */
-exports.get_user = (req, res, next) => {
-
+exports.create_user = (req, res, next) => {
+    
     function start () {
         mysql.use('my_db')
             .query(
-                'SELECT * FROM users WHERE user_id = ? LIMIT 1;',
-                [req.params.id],
+                'INSERT INTO users(email, password, first_name, middle_initial,\
+                                    last_name) \
+                 VALUES(?,?,?,?,?);',
+                [req.body.email, req.body.password, req.body.first_name,
+                 req.body.middle_initial, req.body.last_name],
                 send_response
             )
             .end();
@@ -28,18 +33,13 @@ exports.get_user = (req, res, next) => {
 
     function send_response (err, result, args, last_query) {
         if (err) {
-            winston.error('Error in selecting users', last_query);
+            winston.error('Error in creating user', last_query);
             return next(err);
         }
 
-        if (!result.length) {
-            return res.status(404)
-                .error({code: 'USER404', message: 'User not found'})
+        return res.status(200)
+                .error({message: 'User successfully created'})
                 .send();
-        }
-
-        res.item(result[0])
-            .send();
     }
 
     start();
