134f1ff9e3cbf8f51c028497ab9593899b31b7d7
jgjadaoag <jgj.adaoag@gmail.com>
2016-03-13 07:37:54 +0800

    Fixed and optimized randomize_with_chance

--- helpers/randomize.js
+++ helpers/randomize.js
@@ -7,7 +7,7 @@ function randomize_with_chance(studentList, numberOfVolunteers) {
 	studentList.forEach((student) => fullWeight += student.weight);
 	let selectedWeights = [];
 
-	for(let iii = 0; iii < numberOfVolunteers; iii++) {
+	while (numberOfVolunteers-- > 0) {
 		selectedWeights.push(Math.floor(Math.random()*fullWeight));
 	}
 
@@ -18,15 +18,36 @@ function randomize_with_chance(studentList, numberOfVolunteers) {
 	let currentWeightIndex = 0;
 
 	let size = studentList.push();
-	for(let iii = 0; iii < size; iii++) {
+	for(let iii = 0; iii < size;) {
 		if (selectedWeights[currentWeightIndex] < accumulatedWeight + studentList[iii].weight) {
 			volunteer.push(studentList[iii].student_id);
 			currentWeightIndex++;
+			continue;
 		}
 		accumulatedWeight += studentList[iii].weight;
-	};
+		iii++;
+	}
 
 	return volunteer;
+}
+
+
+function randomize_distinct_with_chance(studentList, numberOfVolunteers) {
+	let fullWeight = 0;
+	let volunteer = [];
+	studentList.forEach((student) => fullWeight += student.weight);
+	while(numberOfVolunteers-- > 0) {
+	}
+
+}
+function randomize(studentList, settings) {
+	if (settings.withChance) {
+		if (settings.unique) {
+			return randomize_distinct_with_chance(studentList, settings.numberOfVolunteers);
+		} else {
+			return randomize_with_chance(studentList, settings.numberOfVolunteers);
+		}
+	}
 }
 
 
