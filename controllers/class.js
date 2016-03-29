ac6fd4285eedde392dd3e048ef72b91238d85c72
lykamacusi <dgmacusi@up.edu.ph>
2016-03-29 15:04:16 +0800

    Fixed issues

--- controllers/class.js
+++ controllers/class.js
@@ -2,6 +2,7 @@
 
 const mysql   = require('anytv-node-mysql');
 const winston = require('winston');
+const csv_writer = require('fast-csv');
 
 /**
  * @api {get} /user/:id Get user information
@@ -86,4 +87,42 @@ exports.delete_class = (req, res, next) => {
     }
 
     start();
+};
+
+exports.write_to_csv = (res, req, next) => {
+
+	function start(){
+		mysql.use('master')
+			.query(
+				'SELECT * FROM student',
+				[],
+				send_response
+			)
+			.end();
+	}
+	
+	function write_to_csv(err, result, args, last_query){
+		if(err){
+			winston.error('Selection query of students failed', lst_query);
+			return.next(err);
+		}
+		
+		csv
+			.writeToPath("students.csv", [
+				result
+			], {headers: true})
+			.on("finish", send_response)
+	}
+	
+	function send_response(err, result, args, last_query){
+	 	if(err){
+	 		winston.error('Could not write to CSV', last_query);
+	 		return.next(err);
+	 	} 
+	 	
+	 	res.item(result[0])
+	 		.send()
+	 }
+	
+	start();
 };
