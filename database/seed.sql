34d198a060fb977d6a7bfc32ffc0093db2c3641e
mnjaverina <mnjaverina@gmail.com>
2016-04-18 15:08:11 +0800

    updated schema.sql and seed.sql

--- database/seed.sql
+++ database/seed.sql
@@ -167,3 +167,15 @@ INSERT INTO student_class(class_id, student_id, times_called)\
 
 INSERT INTO student_class(class_id, student_id, times_called)\
 		VALUES("6","19","1");
+
+--VOLUNTEER seed
+INSERT INTO volunteer(student_id, teacher_id, class_id, volunteer_date)
+		VALUES("1", "4", "6", curdate());
+INSERT INTO volunteer(student_id, teacher_id, class_id, volunteer_date)
+		VALUES("4", "4", "6", curdate());
+INSERT INTO volunteer(student_id, teacher_id, class_id, volunteer_date)
+		VALUES("15", "4", "6", curdate());
+INSERT INTO volunteer(student_id, teacher_id, class_id, volunteer_date)
+		VALUES("18", "3", "3", curdate());
+INSERT INTO volunteer(student_id, teacher_id, class_id, volunteer_date)
+		VALUES("11", "2", "5", curdate());
