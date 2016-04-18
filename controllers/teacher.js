1516c333dd999b5206f1bcd2d8eb8494f224e3a0
mamerisawesome <atmalmer23@gmail.com>
2016-04-18 00:27:26 -0700

    Teacher picture upload done

--- controllers/teacher.js
+++ controllers/teacher.js
@@ -1,7 +1,25 @@
 'use strict';
 
-const mysql   = require('anytv-node-mysql');
-const winston = require('winston');
+const mysql		= require('anytv-node-mysql');
+const winston	= require('winston');
+const sh		= require('shelljs');
+const multer    = require('multer');
+const fs		= require('fs');
+const storage	= multer.diskStorage( {
+    destination: (req, file, cb) => {
+		let destFolder =__dirname + '/../uploads/teachers/pictures';
+
+		if (!fs.existsSync(destFolder)) {
+			fs.mkdirSync(destFolder);
+		}
+
+		cb(null, destFolder);
+    },
+    filename: (req, file, cb) => {
+		cb(null,file.originalname);
+    }
+});
+const upload	= multer({storage : storage}).single('pic');
 
 exports.get_teachers = (req, res, next) => {
 
@@ -13,7 +31,7 @@ exports.get_teachers = (req, res, next) => {
             )
             .end();
     }
-	
+
     function send_response (err, result, args, last_query) {
         if (err) {
             winston.error('Error in getting teachers', last_query);
@@ -44,20 +62,20 @@ exports.post_teacher = (req, res, next) => {
                 'INSERT INTO teacher(email, password, first_name, middle_initial, last_name, picture)'
                 +' VALUES(?,?,?,?,?,?);',
 
-                [req.body.email, req.body.password, req.body.first_name, 
+                [req.body.email, req.body.password, req.body.first_name,
                 req.body.middle_initial, req.body.last_name, req.body.picture],
-                
+
                 send_response
             )
             .end();
     }
-	
+
     function send_response (err, result, args, last_query) {
         if (err) {
             winston.error('Error in creating teacher', last_query);
             return next(err);
         }
-        
+
         res.item(result[0])
             .send();
     }
@@ -66,7 +84,7 @@ exports.post_teacher = (req, res, next) => {
 };
 
 exports.update_teacher = (req, res, next) => {
-	
+
 	function start () {
 		mysql.use('master')
 			.query(
@@ -76,27 +94,27 @@ exports.update_teacher = (req, res, next) => {
 			)
 			.end();
 	}
-	
+
 	function send_response (err, result, args, last_query) {
-		
+
 		if (err) {
 			winston.error('Error in updating teacher', last_query);
 			return next(err);
 		}
-		
+
 		if (!result.affectedRows) {
 			return res.status(404)
 				.error({code: 'teacher404', message: 'teacher not found'})
 				.send();
-		}        
-		
+		}
+
 		res.item(result[0])
 			.send();
 	}
-		
+
 	start();
 };
-	
+
 exports.delete_teacher = (req, res, next) => {
 
 	function start () {
@@ -110,21 +128,60 @@ exports.delete_teacher = (req, res, next) => {
 	}
 
 	function send_response (err, result, args, last_query) {
-		
+
 		if (err) {
 			winston.error('Error in deleting teacher', last_query);
 			return next(err);
 		}
-	
+
 		if (!result.affectedRows) {
 			return res.status(404)
 				.error({code: 'teacher404', message: 'teacher not found'})
 				.send();
 		}
-	
+
 		res.item(result[0])
 			.send();
 	}
-	
+
 	start();
 };
+
+exports.upload_picture = (req, res, next) => {
+
+	function start () {
+		sh.exec('mkdir -p uploads/teachers/', create_pictures_directory);
+	}
+
+	function create_pictures_directory (err) {
+
+		if (err) {
+			winston.error('Error in creating teachers directory');
+			return next(err);
+		}
+
+		sh.exec('mkdir -p uploads/teachers/pictures/', upload_picture);
+	}
+
+	function upload_picture (err) {
+
+		if (err) {
+			winston.error('Error in creating pictures directory');
+			return next(err);
+		}
+
+		upload(req, res, send_response);
+	}
+
+	function send_response (err) {
+
+		if (err) {
+			winston.error('Error in uploading picture');
+			return next(err);
+		}
+
+		res.item(req.file).send();
+	}
+
+	start();
+}
