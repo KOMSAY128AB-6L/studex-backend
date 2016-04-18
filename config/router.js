38622a4ecc769e3384a9549fd4625f3f63d9b389
Tin <annekristinemontoya@gmail.com>
2016-04-18 18:22:09 +0800

    updated route for create_student, added params

--- config/router.js
+++ config/router.js
@@ -20,7 +20,7 @@ module.exports = (router) => {
     router.post('/randomize/classes',               $, __.randomize.randomize_classes);
 
     /*Routes for Student CRUD*/
-    router.post('/student',                         $, __.student.create_student);
+    router.post('/student/:class_id',               $, __.student.create_student);
     router.get ('/student/:id',                     $, __.student.retrieve_student);
     router.get ('/student',                         $, __.student.retrieve_all_student);
     router.get ('/student_logs',                    $, __.student.retrieve_log_of_volunteers);
