8e8017af0605eaa89af37ea06cc8cbbc872a4b5c
camilleshainne <camilleshainne@gmail.com>
2016-04-19 12:47:08 +0800

    Added logger.js

---
+++ helpers/logger.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const mysql   = require('anytv-node-mysql');
+const winston = require('winston');
+
+function log(id, query_log){
+	mysql.use('master')
+		.query(
+			'INSERT INTO history (teacher_id, log_text) VALUES (?,?)',
+			[id, query_log],
+			send_response
+		)
+		.end();
+}
+
+function send_response (err, result, args, last_query){
+	if(err){
+		winston.error('Error in getting teacher', last query);
+		return next(err);
+	}
+	
+	if(!result.affectedRows){
+		return res.status(404)
+				.error({code: 'teacher404', message: 'teacher not found'})
+				.send();
+	}
+	
+	res.send({'message':'Log added.'});
+}
+
+module.exports = {
+	log
+};
