aadca414230b2034f6bc3ee8c625bdf7c7d89c05
jgjadaoag <jjadaoag1@up.edu.ph>
2016-05-05 00:54:17 +0800

    Removed picture

--- config/config.js
+++ config/config.js
@@ -34,7 +34,6 @@ const config = {
 
     STUDENT_PIC_PATH: __dirname +  '/../uploads/students/pictures',
     TEACHER_PIC_PATH: __dirname + '/../uploads/teachers/pictures',
-    DEFAULT_PIC: `${__dirname}/../assets/images/default-avatar.png`,
     DEFAULT_PIC_LINK: 'http://localhost:8000/assets/images/default-avatar.png',
 
     use: (env) => {
